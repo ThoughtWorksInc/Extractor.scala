17f660bb6137663896be1e462f2576e4d63ced61
Yang Bo <atryyang@thoughtworks.com>
2016-11-03 19:43:28 +0800

    Adjust indention

--- shared/src/main/scala/com/thoughtworks/Extractor.scala
+++ shared/src/main/scala/com/thoughtworks/Extractor.scala
@@ -20,8 +20,9 @@ private[thoughtworks] sealed trait LowPriorityExtractor extends LowLowPriorityEx
   sealed trait SeqExtractor[-A, +B] {
     def unapplySeq(a: A): Option[Seq[B]]
   }
-  
-  implicit final class OptionFunctionToSeqExtractor[-A, +B] private[LowPriorityExtractor](underlying: A => Option[Seq[B]]) {
+
+  implicit final class OptionFunctionToSeqExtractor[-A, +B] private[LowPriorityExtractor] (
+      underlying: A => Option[Seq[B]]) {
     def seq = new SeqExtractor[A, B] {
       def unapplySeq(a: A) = underlying(a)
     }
@@ -29,7 +30,7 @@ private[thoughtworks] sealed trait LowPriorityExtractor extends LowLowPriorityEx
     def extractSeq = seq
   }
 
-  implicit final class OptionFunctionToExtractor[-A, +B] private[LowPriorityExtractor](underlying: A => Option[B]) {
+  implicit final class OptionFunctionToExtractor[-A, +B] private[LowPriorityExtractor] (underlying: A => Option[B]) {
     def extract = new Extractor[A, B] {
       def unapply(a: A) = underlying(a)
     }
@@ -51,12 +52,12 @@ sealed trait Extractor[-A, +B] extends (A => Option[B]) {
   * @example
     {{{
     import com.thoughtworks.Extractor._
-    
+
     // Define a PartialFunction
     val pf: PartialFunction[Int, String] = {
       case 1 => "matched by PartialFunction"
     }
-    
+
     // Define an optional function
     val f: Int => Option[String] = { i =>
       if (i == 2) {
@@ -65,10 +66,10 @@ sealed trait Extractor[-A, +B] extends (A => Option[B]) {
         None
       }
     }
-    
+
     // Convert an optional function to a PartialFunction
     val pf2: PartialFunction[Int, String] = f.unlift
-    
+
     util.Random.nextInt(4) match {
       case pf.extract(m) => // Convert a PartialFunction to a pattern
         println(m)
@@ -92,13 +93,13 @@ object Extractor extends LowPriorityExtractor {
     }
   }
 
-  implicit final class PartialFunctionToExtractor[-A, +B] private[Extractor](underlying: PartialFunction[A, B]) {
+  implicit final class PartialFunctionToExtractor[-A, +B] private[Extractor] (underlying: PartialFunction[A, B]) {
     def extract = new Extractor[A, B] {
       def unapply(a: A) = underlying.lift(a)
     }
   }
 
-  implicit final class OptionFunctionToPartialFunction[-A, +B] private[Extractor](underlying: A => Option[B]) {
+  implicit final class OptionFunctionToPartialFunction[-A, +B] private[Extractor] (underlying: A => Option[B]) {
     def unlift: PartialFunction[A, B] = {
       case underlying.extract(b) => b
     }
